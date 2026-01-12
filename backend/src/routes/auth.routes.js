c8a94bbdab53a75c5d36ff062b7195592e814d0b
rachitk29 <rachitkumar2953@gmail.com>
2026-01-12 17:35:20 +0530

    food auth routes setup successfully

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,8 +3,14 @@ const authController = require("../controllers/auth.controller")
 
 const router = express.Router();
 
+// user auth APIs 
 router.post('/user/register',authController.registerUser)
-router.post('/user/register',authController.loginUser)
+router.post('/user/login',authController.loginUser)
 router.post('/user/logout',authController.logoutUser)
+
+// Food Partner auth APIs
+router.post('/food-partner/register', authController.registerFoodPartner)
+router.post('/food-partner/login', authController.loginFoodPartnner)
+router.post('/food-partner/logout', authController.loginFoodPartnner)
 
 module.exports = router;
