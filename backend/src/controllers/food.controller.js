997ec73ac2b69a6db7a20c4331471b1543811d9e
rachitk29 <rachitkumar2953@gmail.com>
2026-01-13 23:56:18 +0530

    food controller setup

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,13 +1,24 @@
 const foodModel = require('../models/food.model');
+const storageService = require('../services/storage.service')
+const { v4: uuid } = require("uuid")
 
 async function createFood(req, res) {
-    
-    console.log(req.foodPartner);
 
-    console.log(req.bod);
-    console.log(req.file);
+    const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid() )
+    // console.log(fileUploadResult);
 
-    res.send("food item created")
+    const foodItem = await foodModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        video: fileUploadResult.url,
+        foodPartner: req.foodPartner._id
+    })
+
+    res.status(201).json({
+        message: "food created successfully",
+        food: foodItem
+    })
+    // res.send("food item created")
 }
 
 module.exports = {
