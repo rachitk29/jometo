4ab606e129c2a4f20963220ee27de96535b55a4c
rachitk29 <rachitkumar2953@gmail.com>
2026-01-17 15:54:44 +0530

    fixed the project structure

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import UserLogin from '../pages/UserLogin';
-import UserRegister from '../pages/UserRegister';
-import FoodPartnerLogin from '../pages/FoodPartnerLogin';
-import FoodPartnerRegister from '../pages/FoodPartnerRegister';
-import Home from '../general/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "../pages/general/Home";
+import UserLogin from "../pages/auth/UserLogin";
+import UserRegister from "../pages/auth/UserRegister";
+import FoodPartnerLogin from "../pages/auth/FoodPartnerLogin";
+import FoodPartnerRegister from "../pages/auth/FoodPartnerRegister";
+
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/user/register" element={<UserRegister />} />
         <Route path="/user/login" element={<UserLogin />} />
         <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
         <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
-        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
